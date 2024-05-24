bb6566eb0f31434c77870b6f84ef971095bcb3b3
harryprayiv <harryprayiv@gmail.com>
2024-05-24 11:16:26 -0400

    working with line delimited json!  error

--- src/Main.hs
+++ src/Main.hs
@@ -7,15 +7,18 @@ import System.FilePath
 import Control.Monad
 import Data.List (sortOn)
 import Data.Maybe (mapMaybe, isNothing)
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
 import Conduit (yieldMany, mapC, (.|), runConduitRes)
 import Data.Conduit.Binary (sinkFileCautious)
 import Text.XML (Document, readFile, def)
-import Data.Aeson (encode) -- Importing encode from Aeson
+import Data.Aeson (encode)
 import Movies
 import NFO
 import JSON
+import Control.Monad.Trans.Resource (MonadThrow)
+import Control.Monad.IO.Class (liftIO)
 
 data Options = Options
   { directory :: FilePath
@@ -72,9 +75,22 @@ processDirectory dir = do
   forM_ movies $ \movie -> putStrLn $ "Found movie: " ++ title movie
 
   let sortedMovies = sortOn title movies
-  runConduitRes $ yieldMany sortedMovies .| mapC (BS.concat . B.toChunks . encode) .| sinkFileCautious (dir </> "library.json")
+
+  -- Debugging: print the number of sorted movies
+  putStrLn $ "Writing " ++ show (length sortedMovies) ++ " movies to library.json"
+
+  runConduitRes $
+    yieldMany sortedMovies
+    .| mapC (\m -> let encoded = encode m in BS.concat (BL.toChunks encoded) `BS8.append` BS8.singleton '\n')
+    .| sinkFileCautious (dir </> "library.json")
+
   putStrLn $ "Library JSON written to: " ++ (dir </> "library.json")
-  runConduitRes $ yieldMany missingNfoFiles .| mapC (BS.concat . B.toChunks . encode) .| sinkFileCautious (dir </> "unNamed.json")
+
+  runConduitRes $
+    yieldMany missingNfoFiles
+    .| mapC (\f -> let encoded = encode f in BS.concat (BL.toChunks encoded) `BS8.append` BS8.singleton '\n')
+    .| sinkFileCautious (dir </> "unNamed.json")
+
   putStrLn $ "Missing NFO JSON written to: " ++ (dir </> "unNamed.json")
 
 processNfoFile :: FilePath -> IO (Either String Movie)
@@ -85,4 +101,15 @@ processNfoFile filePath = do
     Left ex -> do
       putStrLn $ "Error reading file: " ++ filePath ++ " - " ++ show ex
       return $ Left filePath
-    Right doc -> return $ Right (parseNfo doc)
+    Right doc -> 
+      case parseNfoSafe doc of
+        Just movie -> return $ Right movie
+        Nothing -> do
+          putStrLn $ "Failed to parse NFO file: " ++ filePath
+          return $ Left filePath
+
+parseNfoSafe :: Document -> Maybe Movie
+parseNfoSafe doc = 
+  case parseNfo doc of
+    Just movie -> Just movie
+    Nothing -> Nothing
--- src/NFO.hs
+++ src/NFO.hs
@@ -1,79 +1,91 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 module NFO where
 
 import Text.XML
 import Text.XML.Cursor
 import Data.Text (Text, unpack)
 import qualified Data.Text as T
-import Conduit (yieldMany, mapC)
-import Data.Conduit.Binary (sinkFileCautious)
+import Text.Read (readMaybe)
+import Data.Maybe (listToMaybe)
+
 import Movies
 
-parseNfo :: Document -> Movie
-parseNfo doc = Movie
-  { title = getElemText "title"
-  , originalTitle = getElemText "originaltitle"
-  , sortTitle = getElemText "sorttitle"
-  , year = read (getElemText "year") :: Int
-  , rating = read (getElemText "rating") :: Double
-  , votes = read (getElemText "votes") :: Int
-  , mpaa = getElemText "mpaa"
-  , movieId = getElemText "id"
-  , plot = getElemText "plot"
-  , tagline = getElemText "tagline"
-  , runtime = read (getElemText "runtime") :: Int
-  , genres = getElemsText "genre"
-  , tags = getElemsText "tag"
-  , countries = getElemsText "country"
-  , studios = getElemsText "studio"
-  , directors = getElemsText "director"
-  , credits = getElemsText "credits"
-  , fileInfo = getFileInfo
-  , imdbId = getAttrText "imdb" "id"
-  , tmdbId = getAttrText "tmdb" "id"
-  , actors = []  -- Initialize actors as an empty list
+parseNfo :: Document -> Maybe Movie
+parseNfo doc = do
+  let cursor = fromDocument doc
+  fileInfo <- parseFileInfo =<< listToMaybe (cursor $// element "fileinfo")
+  return Movie
+    { title = getElemText cursor "title"
+    , originalTitle = getElemText cursor "originaltitle"
+    , sortTitle = getElemText cursor "sorttitle"
+    , year = readElemText cursor "year" 0
+    , rating = readElemText cursor "rating" 0.0
+    , votes = readElemText cursor "votes" 0
+    , mpaa = getElemText cursor "mpaa"
+    , movieId = getElemText cursor "id"
+    , plot = getElemText cursor "plot"
+    , tagline = getElemText cursor "tagline"
+    , runtime = readElemText cursor "runtime" 0
+    , genres = getElemsText cursor "genre"
+    , tags = getElemsText cursor "tag"
+    , countries = getElemsText cursor "country"
+    , studios = getElemsText cursor "studio"
+    , directors = getElemsText cursor "director"
+    , credits = getElemsText cursor "credits"
+    , fileInfo = fileInfo
+    , imdbId = getAttrText cursor "imdb" "id"
+    , tmdbId = getAttrText cursor "tmdb" "id"
+    , actors = []  -- Initialize actors as an empty list
+    }
+  where
+    getElemText cur name = T.unpack $ T.concat $ cur $// element name &// content
+    getElemsText cur name = map T.unpack $ cur $// element name &// content
+    getAttrText cur elemName attrName = T.unpack $ T.concat $ cur $// element elemName >=> attribute attrName
+    readElemText cur name def = maybe def id . readMaybe . getElemText cur $ name
+
+parseFileInfo :: Cursor -> Maybe FileInfo
+parseFileInfo cur = do
+  streamDetails <- parseStreamDetails =<< listToMaybe (cur $// element "streamdetails")
+  return FileInfo
+    { streamDetails = streamDetails
+    }
+
+parseStreamDetails :: Cursor -> Maybe StreamDetails
+parseStreamDetails cur = do
+  video <- parseVideo =<< listToMaybe (cur $// element "video")
+  let audios = map parseAudio (cur $// element "audio")
+  let subtitles = map parseSubtitle (cur $// element "subtitle")
+  return StreamDetails
+    { video = video
+    , audios = audios
+    , subtitles = subtitles
+    }
+
+parseVideo :: Cursor -> Maybe Video
+parseVideo cur = Just Video
+  { v_codec = getElemText cur "codec"
+  , aspect = readElemText cur "aspect" 0.0
+  , width = readElemText cur "width" 0
+  , height = readElemText cur "height" 0
   }
   where
-    cursor = fromDocument doc
-    getElemText name = T.unpack $ T.concat $ cursor $// element name &// content
-    getElemsText name = map T.unpack $ cursor $// element name &// content
-    getAttrText elemName attrName = T.unpack $ T.concat $ cursor $// element elemName >=> attribute attrName
-    getFileInfo = parseFileInfo $ head $ cursor $// element "fileinfo"
-
-parseFileInfo :: Cursor -> FileInfo
-parseFileInfo cur = FileInfo
-  { streamDetails = parseStreamDetails $ head $ cur $// element "streamdetails"
-  }
-
-parseStreamDetails :: Cursor -> StreamDetails
-parseStreamDetails cur = StreamDetails
-  { video = parseVideo $ head $ cur $// element "video"
-  , audios = map parseAudio $ cur $// element "audio"
-  , subtitles = map parseSubtitle $ cur $// element "subtitle"
-  }
-
-parseVideo :: Cursor -> Video
-parseVideo cur = Video
-  { v_codec = getElemText "codec"
-  , aspect = read (getElemText "aspect") :: Double
-  , width = read (getElemText "width") :: Int
-  , height = read (getElemText "height") :: Int
-  }
-  where
-    getElemText name = T.unpack $ T.concat $ cur $// element name &// content
+    getElemText cur name = T.unpack $ T.concat $ cur $// element name &// content
+    readElemText cur name def = maybe def id . readMaybe . getElemText cur $ name
 
 parseAudio :: Cursor -> Audio
 parseAudio cur = Audio
-  { a_codec = getElemText "codec"
-  , a_language = getElemText "language"
-  , channels = read (getElemText "channels") :: Int
+  { a_codec = getElemText cur "codec"
+  , a_language = getElemText cur "language"
+  , channels = readElemText cur "channels" 0
   }
   where
-    getElemText name = T.unpack $ T.concat $ cur $// element name &// content
+    getElemText cur name = T.unpack $ T.concat $ cur $// element name &// content
+    readElemText cur name def = maybe def id . readMaybe . getElemText cur $ name
 
 parseSubtitle :: Cursor -> Subtitle
 parseSubtitle cur = Subtitle
-  { s_language = getElemText "language"
+  { s_language = getElemText cur "language"
   }
   where
-    getElemText name = T.unpack $ T.concat $ cur $// element name &// content
+    getElemText cur name = T.unpack $ T.concat $ cur $// element name &// content
